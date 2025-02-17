c7a30272e9d1547638beec68c604bd108585f26c
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2025-02-17 22:40:59 +0530

    Update Reverse.cpp

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,1 +1,20 @@
-
+#include<iostream>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        long int reversed = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            reversed = reversed * 10 + digit;
+            if (reversed > INT_MAX || reversed < INT_MIN) {
+                return 0;
+            }
+        }
+        return static_cast<int>(reversed);
+    }
+};
+int main()
+{
+  
