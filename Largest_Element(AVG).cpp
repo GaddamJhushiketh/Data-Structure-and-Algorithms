ca4844345fc507546bd7f956dffa89ea2a4a0705
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2025-02-24 15:14:58 +0530

    Update Largest_Element(AVG).cpp

--- Largest_Element(AVG).cpp
+++ Largest_Element(AVG).cpp
@@ -18,3 +18,26 @@ class Solution {
         return largest;
     }
 };
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.largest(arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
