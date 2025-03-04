c842d10d06e4a1db2a6938998b9780ec9b35c54b
Jhushiketh Gaddam <gjhushiketh@gmail.com>
2025-03-04 15:49:16 +0530

    Add files via upload

---
+++ Finding_missing_num_brute.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+	int n,j=0;
+	cout<<"Enter the size of array :";
+	cin>>n;
+	vector<int> arr(n);
+	cout<<"Enter the Elements in to array: ";
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+    for(int i=0;i<n;i++)
+    {
+    	int flag=0;
+    	for(int j=0;j<n;j++)
+    	{
+		if(arr[j]==i)
+		{
+			flag=1;
+		}	
+		}
+		if(flag==0)
+		{
+			cout<<"Missing num: "<<i;
+			break;
+		}
+	}
+}
